8a14a53c84a986a1ccdc07b9319be73f547a1967
AroSwift <abarlow505@gmail.com>
2016-06-05 19:16:49 -0400

    Added logo and custom css for top nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  # http://liamkaufman.com/blog/2013/02/27/adding-real-time-to-a-restful-rails-app/
+  # https://github.com/ankane/searchkick
+
   def index
     @users = User.all
     # render 'layouts/application'
