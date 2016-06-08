0bde9568fc70d33152cc1d9f74831c7634bb371a
AroSwift <abarlow505@gmail.com>
2016-06-08 00:25:21 -0400

    Add post routes and general use content classes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
-    get :signup, on: :collection
-    get :login, on: :collection
+    get  :kid_signup, on: :collection
+    post :kid_signup, on: :collection
+    get  :parent_signup, on: :collection
+    post :parent_signup, on: :collection
+    get  :login, on: :collection
+    post :login, on: :collection
   end
 
   root 'users#index'
